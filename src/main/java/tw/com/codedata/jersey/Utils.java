458208d533070ad84a692e1b0218cab08b6aa0fa
ray wu <superwmr@gmail.com>
2019-01-17 14:01:18 +0800

    no message

--- src/main/java/tw/com/codedata/jersey/Utils.java
+++ src/main/java/tw/com/codedata/jersey/Utils.java
@@ -30,7 +30,7 @@ public class Utils {
 		clearConsole();
 		
 		System.out.println("============================================================");
-		System.out.println("1:回傳註冊結果, 2:轉帳, 3:取得餘額");
+		System.out.println("1:回傳註冊結果, 2:轉帳, 3:取得餘額, 4:登出");
 		System.out.println("formet:CMD 銀行 帳號 電話 名字 id_card 密碼 轉帳密碼 金額 對方銀行 對方帳號 對方名字 order_id job_id");
 		System.out.println("ex:2 302 6217732101295032 18114899095 明廷良 id_card aa343599 188188 1 101 6217234301008178893 王露 order1234 job5678");
 		//
